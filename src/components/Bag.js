4ef13346bd46f6d319213fe0fb13c4896e17d085
BEJI <119804271+MED27BJ@users.noreply.github.com>
2023-05-28 11:00:38 +0100

    REGLAGE INBAG

---
+++ src/components/Bag.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+
+const Bag = () => {
+    const bookData = useSelector((state) => state.bookData.BookData);
+  
+    return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Bag
--- src/components/InBag.js
+++ src/components/InBag.js
@@ -1,35 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux';
 import "../styles/BookCard.css"
 const InBag = () => {
-    const el=useSelector((state)=>state.bookData.BookData);
-  console.log(el)
+    const bookData=useSelector((state)=>state.bookData.BookData);
+  console.log(bookData)
+
     return (
-    <div>
+    <div>{bookData.filter((el)=>el.isInBag===true)
+  
+      .map((item) => (
     <main role="main">
     <div className="product">
       <figure>
-        <img src={el[0].posterURL} alt="Product Image" className="product-image"/>
+        <img src={item.posterURL} alt="Product Image" className="product-image"/>
           </figure>
   
         <div className="product-description">
   
           <div className="info">
-            <h1>{el[0].title}</h1>
+            <h1>{item.title}</h1>
             <h3>
-              {el[0].writer}
+              {item.writer}
             </h3>
             <h3>
-              {el[0].theme}
+              {item.theme}
             </h3>
           </div>
   
           <div className="price">
-            {el[0].price}
+            {item.price}
           </div>
         </div>
         </div>
-      </main>
+      </main>))}
     </div>
   )
 }
